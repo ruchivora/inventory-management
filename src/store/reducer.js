fb6fe340f937463d4cf2f94b25d45a69c3b67baf
Ruchi Rameshkumar Vora <ruchivora@Ruchis-MacBook-Pro.local>
2024-10-28 09:21:37 +0530

    update local storage

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,4 +1,5 @@
 import { SET_INVENTORY, DELETE_ITEM, UPDATE_ITEM, TOGGLE_DISABLED_STATUS, SET_VIEW_TYPE } from './actionTypes';
+const LOCALSTORAGE_KEY = 'inventoryData';
 
 export const initialState = {
   inventoryList: [],
@@ -10,16 +11,17 @@ export function inventoryReducer(state, action) {
   switch (action.type) {
 
   case SET_INVENTORY: {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(action.payload));
     return { ...state, inventoryList: [...action.payload] };
   }
     
 
   case DELETE_ITEM: {
     const updatedList = state.inventoryList.filter(item => item.id !== action.payload);
-    
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(updatedList));
     return {
       ...state,
-      inventoryList: updatedList,
+      inventoryList: [...updatedList],
     };
   }
     
@@ -29,36 +31,39 @@ export function inventoryReducer(state, action) {
       (item.id === action.payload.id) ? { ...item, ...action.payload } : item
     );
 
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(updatedList));
     return {
       ...state,
-      inventoryList: updatedList
+      inventoryList: [...updatedList]
     };
   }
     
 
   case TOGGLE_DISABLED_STATUS: {
-    const updateList = state.inventoryList.map(item => 
+    const updatedList = state.inventoryList.map(item => 
       item.id === action.payload ? { ...item, isDisabled: !item.isDisabled } : item
     )
 
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(updatedList));
     return {
       ...state,
-      inventoryList: updateList
+      inventoryList: [...updatedList]
     };
   }
     
 
   case SET_VIEW_TYPE: {
     const isAdminView = (action.payload === 'Admin');
-    const updateList = state.inventoryList.map(item => ({
+    const updatedList = state.inventoryList.map(item => ({
       ...item,
       isDisabled: !isAdminView,
     }))
-
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(updatedList));
+    
     return {
       ...state,
       isAdmin: isAdminView,
-      inventoryList: updateList
+      inventoryList: [...updatedList]
     };
   }
     
